b9e91201aeec5a4f82e0133054228df97d04fad4
+hannah porter <hannah.porter1995@hotmail.com>
2024-01-21 16:02:56 +1100

    done

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+"Hello!".toUpperCase(); // 'HELLO!';
+
+
+function whisper(string) {
+return string.toLowerCase();
+}
+'HELLO'.toLowerCase()
+
+function logShout (HELLO) {
+    return HELLO.toUpperCase( );
+    }
+
+    function logShout(string) {
+        console.log(string.toUpperCase())
+      }
+    
+      function logWhisper(string) {
+        console.log(string.toLowerCase())
+      }
+  
+function sayHiToHeadphonedRoommate(string) {
+    if  (string === "Let's have dinner together!"){
+      return "I would love to!" 
+    }
+    else if (string === string.toLowerCase()){
+      return "I can't hear you!";
+    }
+     else (string !== string.toLowerCase());{
+      return "YES INDEED!";
+    } 
+}
